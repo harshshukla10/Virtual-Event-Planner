a57d6f6cb6f642939733fc1571fac4575bbfa903
Delta Student <ww@gmail.com>
2025-02-26 19:56:23 +0530

    added files

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 8080;
+const port = process.env.PORT|| 8080;
 const path = require("path");
 const User = require("./models/model1.js");
 const dashData = require("./models/dashdata.js");
